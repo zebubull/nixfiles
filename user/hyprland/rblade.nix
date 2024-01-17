2d709ba2186dad691ca9eff85ea37cb18a201fde
zebubull <ldkourtev@gmail.com>
2024-01-17 12:31:39 -0500

    fixed something

--- user/hyprland/rblade.nix
+++ user/hyprland/rblade.nix
@@ -30,9 +30,13 @@
     input = {
       kb_layout = "us";
       follow_mouse = 0;
-      touchpad.natural_scroll = "no";
       sensitivity = 0.0;
       kb_options = "ctrl:nocaps";
+      touchpad = {
+        natural_scroll = "no";
+        tap-to-click = "no";
+        clickfinger_behavior = true;
+      };
     };
     decoration = {
       blur = {
