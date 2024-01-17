986e2b2e16d6f51a01fc61b05a6b63b6d176823d
ldkourtev@gmail.com <ldkourtev@gmail.com>
2024-01-16 23:07:15 -0500

    media keys

--- user/hyprland/hyprland.nix
+++ user/hyprland/hyprland.nix
@@ -90,7 +90,14 @@
       "$mainMod, l, movefocus, r"
 
       ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
     ]
+    bindle = [
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%+"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%-"
+      ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+      ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+    ];
     ++ (
       # workspace bindings
       builtins.concatLists (builtins.genList (
