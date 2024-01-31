e0d2ef35fed536305c462314c031f1c2f3b18e37
zebubull <ldkourtev@gmail.com>
2024-01-31 10:19:24 -0500

    fish prompt

--- user/common.nix
+++ user/common.nix
@@ -22,16 +22,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/fish/functions/fish_prompt.fish".source = fish/fish_prompt.fish;
   };
 
   home.sessionVariables = {
@@ -46,6 +37,9 @@
       la = "ls -la";
       dev = "nix develop --command fish";
     };
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
   };
 
   programs.git = {
