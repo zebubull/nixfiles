72b26fe8a25c8f14469a1bb0748e6c98a6982816
ldkourtev@gmail.com <ldkourtev@gmail.com>
2024-06-11 03:39:33 -0400

    unleak my api keys

--- eww/dashboard/scripts/mails.py
+++ eww/dashboard/scripts/mails.py
@@ -1,5 +1,5 @@
 import imaplib
 obj = imaplib.IMAP4_SSL('imap.gmail.com',993)
-obj.login('ldkourtev@gmail.com','rljn qsbh vrvl ogsw ') # write your email and password
+obj.login('EMAIL', 'PASSWORD') # write your email and password
 obj.select()
 print(len(obj.search(None, 'UnSeen')[1][0].split()))
