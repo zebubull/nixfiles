845502541dca73fa08c8eb247ec0f1f4118f240a
zebubull <ldkourtev@gmail.com>
2024-06-11 04:38:31 -0400

    fix eww stuff

--- user/hyprland/rblade.nix
+++ user/hyprland/rblade.nix
@@ -12,7 +12,6 @@
     "eDP-1, 1920x1080@144,0x0,1.0"
     ];
     exec-once = [
-      "waybar"
       "mako"
       "wl-paste --type text --watch cliphist store #Stores only text data"
       "wl-paste --type image --watch cliphist store #Stores only image data"
@@ -25,6 +24,7 @@
       "swww init"
       "swayidle -w before-sleep 'swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --ring-color 584678 --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 0 --fade-in 0.2'"
       "lxqt-policykit-agent"
+      "/home/zebubull/.config/eww/bar/launch_bar"
      ];
     env = [
       "XCURSOR_SIZE,24"
@@ -41,6 +41,9 @@
         tap-to-click = "no";
         clickfinger_behavior = true;
       };
+    };
+    debug = {
+      disable_logs = false;
     };
     decoration = {
       blur = {
